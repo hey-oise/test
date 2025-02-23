c909cd3b25a349f0bdd2d453c1a484ccd57b1b12
hey-oise <oise.giwa.okugbe@gmail.com>
2025-02-23 15:40:07 +0100

    init

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+
+
+const Person = (props) => {
+  return (
+    <>
+      <h1>name: {props.name}</h1>
+      <h2>Last name: {props.LastName}</h2>
+      <h2>Age: 30</h2>
+    </>
+  );
+};
+
+export default function MyApp() {
+  const [counter, setCounter] = useState(0);
+
+  // useEffect(() => {
+  //   alert("reload");
+  // });
+  return (
+    <div>
+      {/* <Person name={"oise"} LastName={"doe"} />
+      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>
+        -
+      </button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>
+        +
+      </button> */}
+
+      <button
+        className="eg"
+        onClick={() => setCounter((prevCount) => prevCount + 1)}>{counter}</button>
+    </div>
+  );
+}
