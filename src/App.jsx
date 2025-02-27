8b98f38ef468cff5d66eba287d80c9d7bcb7225a
hey-oise <oise.giwa.okugbe@gmail.com>
2025-02-27 16:03:41 +0100

    inint

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 
-
 const Person = (props) => {
   return (
     <>
       <h1>name: {props.name}</h1>
       <h2>Last name: {props.LastName}</h2>
-      <h2>Age: 30</h2>
+      <h2>Age: {props.Age}</h2>
     </>
   );
 };
@@ -20,18 +19,22 @@ export default function MyApp() {
   // });
   return (
     <div>
-      {/* <Person name={"oise"} LastName={"doe"} />
+      <Person name={"oise"} LastName={"doe"} Age={30} />
+      <Person LastName = 'doe'/>
       <button onClick={() => setCounter((prevCount) => prevCount - 1)}>
         -
       </button>
       <h1>{counter}</h1>
-      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>
+      <button onClick={() => setCounter((prevCount) => prevCount + 1-2)}>
         +
-      </button> */}
+      </button>
 
-      <button
+       <button
         className="eg"
-        onClick={() => setCounter((prevCount) => prevCount + 1)}>{counter}</button>
+        onClick={() => setCounter((prevCount) => prevCount + 10000000000000000000000)}
+      >
+        {counter}
+      </button>
     </div>
   );
 }
